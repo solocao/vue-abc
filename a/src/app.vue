8987eb9c1eda50d65fa78fd18d0e69e9980f9875
solocao <solocao@126.com>
2017-09-19 23:51:52 +0800

    新建项目

---
+++ a/src/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg: 'Hello World!'
+        }
+    }
+}
+</script>
+
+<style scoped>
+#app {
+   background:yellow
+}
+</style>
